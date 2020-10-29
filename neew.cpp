e6eaae9211be6c13a4288cb62cbd55e031106a08
NursuluOspan <63782092+NursuluOspan@users.noreply.github.com>
2020-10-29 15:29:37 +0600

    Индекс түрлендіру функциясы және оны шақыру

--- neew.cpp
+++ neew.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int f(int i, int j, int cols)
-{
+//екі өлшемді->бір өлшемді
+int f(int i, int j, int cols){
 	
     return i * cols + j;
 }
+//бір өлшемді->екі өлшемді
 int f2(int i, int cols){
 	
 	cout<<"["<<i/4<<"]["<<i%4<<"]"<<endl;
@@ -15,11 +16,8 @@ int f2(int i, int cols){
 int main(){ 
 
 //b[3][4]=a[f(i,j,cols)]
-
-
 cout<<"[0][0]->["<<f(0,0,4)<<"]"<<endl;
 cout<<"[2][1]->["<<f(2,1,4)<<"]"<<endl;
-
 
 //a[3*4]=b[i/4][i%4];
 cout<<"[5]->";
