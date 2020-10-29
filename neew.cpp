8e04527333a5d239bc7fe971a79c4656bc63a804
NursuluOspan <63782092+NursuluOspan@users.noreply.github.com>
2020-10-29 15:27:32 +0600

    Update neew.cpp

--- neew.cpp
+++ neew.cpp
@@ -3,10 +3,28 @@
 using namespace std;
 int f(int i, int j, int cols)
 {
+	
     return i * cols + j;
 }
+int f2(int i, int cols){
+	
+	cout<<"["<<i/4<<"]["<<i%4<<"]"<<endl;
+	return 0;
+}
+
 int main(){ 
-int b[3][4];
-int a[3*4];
-cout<<f(1,2,4);
+
+//b[3][4]=a[f(i,j,cols)]
+
+
+cout<<"[0][0]->["<<f(0,0,4)<<"]"<<endl;
+cout<<"[2][1]->["<<f(2,1,4)<<"]"<<endl;
+
+
+//a[3*4]=b[i/4][i%4];
+cout<<"[5]->";
+f2(5,4);
+cout<<"[11]->";
+f2(11,4);
+
 }
